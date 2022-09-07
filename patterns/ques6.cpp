203d26fb8b0d9bd444711346fa38f5de02ed757e
Niharika Dhyani <87805219+Niharika-012@users.noreply.github.com>
2022-09-07 22:30:26 +0530

    Create ques6.cpp
    Printing this type of  pattern:
    
    1 2 3 4 5
    1 2 3 4
    1 2 3
    1 2
    1

---
+++ patterns/ques6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n)
+{
+    int i , j;
+    for(i = 0 ; i<n ; i++)
+    {
+        for(j=0 ; j<n-i ; j++)
+        {
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{   
+  int n ;
+  cin>>n;
+
+  pattern(n);
+    return 0;
+}
